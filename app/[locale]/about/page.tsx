7d4a14affeda05b1fdfcf399c4a70d87da0cf1a7
ali007-depug <aliroma849@gmail.com>
2026-01-17 21:35:19 +0200

    fix about page delay

--- app/[locale]/about/page.tsx
+++ app/[locale]/about/page.tsx
@@ -2,8 +2,12 @@ import About from "@/app/ui/components/AboutPage/About";
 import Sidepanel from "@/app/ui/components/SidePanel";
 import { getTranslations } from "next-intl/server";
 
-export default async function AboutMe({ locale }: { locale: string }) {
-  // const t = useTranslations();
+export default async function AboutMe({
+  params,
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
   const t = await getTranslations({ namespace: "aboutPage", locale });
   return (
     <section className="px-dyp py-5 md:py  relative top-[76px] sm:max-md:top-[111px]  bg-p-color">
