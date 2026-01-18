b781ad3e5536112bee7f3836bab05c1d7f4a1e2e
ali007-depug <aliroma849@gmail.com>
2026-01-18 15:25:44 +0200

    make all route is SSG

--- app/[locale]/skills/page.tsx
+++ app/[locale]/skills/page.tsx
@@ -1,16 +1,19 @@
 import NextLevel from "@/app/ui/components/skillPage/NextLevel";
 import SkillHeader from "@/app/ui/components/skillPage/skillHeader";
 import SkillsBadges from "@/app/ui/components/SkillsBadges";
-export default function MySkills({ locale }: { locale: string }) {
+export default async function MySkills({
+  params
+}: {
+  params: Promise<{ locale: string }>;
+}) {
+  const { locale } = await params;
   return (
-    <>
-      <section className="px-dyp py-5 md:py  relative top-[76px] sm:max-md:top-[111px] bg-p-color">
-        <SkillHeader locale={locale} />
-        {/* skill badges */}
-        <SkillsBadges isLearntSkills={true} bg={"bg-sky-900"} />
-        {/* Next Level */}
-        <NextLevel locale={locale} />
-      </section>
-    </>
+    <section className="px-dyp py-5 md:py  relative top-[76px] sm:max-md:top-[111px] bg-p-color">
+      <SkillHeader locale={locale} />
+      {/* skill badges */}
+      <SkillsBadges isLearntSkills={true} bg={"bg-sky-900"} />
+      {/* Next Level */}
+      <NextLevel locale={locale} />
+    </section>
   );
 }
--- app/[locale]/works/page.tsx
+++ app/[locale]/works/page.tsx
@@ -1,7 +1,8 @@
 import ProjectsTaps from "@/app/ui/components/worksPage/ProjectsTaps";
 import WorksHeader from "@/app/ui/components/worksPage/WorksHeader";
 
-export default function MyWorks({ locale }: { locale: string }) {
+export default async function MyWorks({ params } :{params :Promise<{ locale: string }>}) {
+  const { locale } = await params;
   return (
       <section className="px-dyp py-5 md:py  relative top-[76px] sm:max-md:top-[111px]">
         <WorksHeader locale={locale} />
