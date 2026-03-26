ab010b6ae4095d659303edd2a7fd005ac57e0c0a
ali007-depug <aliroma849@gmail.com>
2026-03-26 12:21:21 +0200

    add revalidation for posts using webhooks

---
+++ app/api/revalidate/route.ts
@@ -0,0 +1,21 @@
+import { revalidatePath, revalidateTag } from 'next/cache';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const secret = request.nextUrl.searchParams.get('secret');
+
+  // Check for a secret to prevent unauthorized people from purging your cache
+  if (secret !== process.env.CONTENTFUL_REVALIDATE_SECRET) {
+    return NextResponse.json({ message: 'Invalid secret' }, { status: 401 });
+  }
+
+  try {
+    // This will purge the cache for your blog list
+    // You can also use revalidateTag('posts') if you tagged your fetch
+    revalidatePath('/[locale]/blog', 'layout'); 
+    
+    return NextResponse.json({ revalidated: true, now: Date.now() });
+  } catch (err) {
+    return NextResponse.json({ message: 'Error revalidating' }, { status: 500 });
+  }
+}
