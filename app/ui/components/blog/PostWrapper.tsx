9f26480e137f180abb17bdba9a093de8845b51be
ali007-depug <aliroma849@gmail.com>
2026-03-26 12:21:32 +0200

    add revalidation for posts using webhooks

--- app/ui/components/blog/PostWrapper.tsx
+++ app/ui/components/blog/PostWrapper.tsx
@@ -1,7 +1,6 @@
 import getAllPosts from "@/app/actions/getAllPosts";
 import Posts from "./Posts";
 
-export const revalidate = 60; // Revalidate every 60 seconds
 
 export default async function PostWrapper({locale}:{locale:string}){
 const data = await getAllPosts({ locale });
