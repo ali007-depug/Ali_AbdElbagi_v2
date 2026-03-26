1ca9f357a25fd11927bd67cc9ca9b9ff4bbcd686
ali007-depug <aliroma849@gmail.com>
2026-03-26 11:43:30 +0200

    add revalidation for posts

--- app/actions/getAllPosts.tsx
+++ app/actions/getAllPosts.tsx
@@ -1,4 +1,5 @@
 import { client } from "../lib/contentful";
+
 export default async function getAllPosts({ locale }: { locale: string }) {
   const response = await client.getEntries({
     content_type: "blog",
