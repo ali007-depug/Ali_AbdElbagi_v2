40f92e42f4921decdcec8a7ffec69ce6d803a7e4
ali007-depug <aliroma849@gmail.com>
2026-08-01 10:20:36 +0200

    fix skills page mobile overflow

--- src/app/[locale]/works/_components/ProjectsTaps.tsx
+++ src/app/[locale]/works/_components/ProjectsTaps.tsx
@@ -6,7 +6,7 @@ export default function ProjectsTaps() {
   return (
     <ProjcetsProvider>
       <TapList />
-      <div className="grid grid-cols-[repeat(auto-fit,minmax(350px,1fr))] gap-[15px]">
+      <div className="grid grid-cols-[repeat(auto-fit,minmax(300px,1fr))] gap-[15px]">
         <WorkCards numberOfCards={20} />
       </div>
     </ProjcetsProvider>
