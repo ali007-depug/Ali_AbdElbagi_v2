c01b63a8dcdc68ede01cf88dc6e84b595811fa49
ali007-depug <aliroma849@gmail.com>
2026-09-25 17:09:11 +0200

    refactor: permanently remove legacy components and actions in favor of src/features

--- src/actions/getPostTags.tsx
+++
@@ -1,9 +0,0 @@
-import { client } from "../lib/contentful";
-export default async function fetchPostsByTag(tag: string, locale: string) {
-  const response = await client.getEntries({
-    content_type: "blog",
-    "fields.tag[in]": tag,
-    locale: locale,
-  });
-  return response.items;
-}
--- src/app/[locale]/blog/_components/PostWrapper.tsx
+++
@@ -1,6 +0,0 @@
-import getAllPosts from "@/src/actions/getAllPosts";
-import Posts from "./Posts";
-export default async function PostWrapper({ locale }: { locale: string }) {
-  const data = await getAllPosts({ locale });
-  return <Posts data={data} />;
-}
